30a72091b3739a40191f35fe5c6c615da95c71f8
Nimyaah24 <nimyapappy823@gmail.com>
2024-07-20 20:22:58 +0530

    dashboard added

--- src/components/CategorizedView.jsx
+++ src/components/CategorizedView.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+
+import NewsCards from './NewsCards';
 
 function CategorizedView() {
     const [isSelected, setIsSelected] = useState("all")
@@ -22,56 +22,7 @@ function CategorizedView() {
                 </div>
             </div>
 
-
-
-            <div className="row w-100 mt-3 mb-2 d-flex justify-content-center align-items-center p-4 ">
-
-
-
-                <div className="col-md-4 p-4 d-flex justify-content-center align-items-center">
-                    <Card style={{ width: "100% " }} className='p-2 shadow'>
-                        <Card.Body>
-                            <Card.Title>Card Title</Card.Title>
-                            <Card.Text className='mt-3'>
-                                Some quick example text to build on the card title and make up the
-                                bulk of the card's content.
-                            </Card.Text>
-                            <Button variant="danger" className='mt-2'>View More</Button>
-                        </Card.Body>
-                    </Card></div>
-
-                <div className="col-md-4 p-4 d-flex justify-content-center align-items-center">
-                    <Card style={{ width: "100%  " }} className='p-2 shadow'>
-
-                        <Card.Body>
-                            <Card.Title>Card Title</Card.Title>
-                            <Card.Text className='mt-3'>
-                                Some quick example text to build on the card title and make up the
-                                bulk of the card's content.
-                            </Card.Text>
-                            <Button variant="danger" className='mt-2'>View More</Button>
-                        </Card.Body>
-                    </Card>
-                </div>
-
-                <div className="col-md-4 p-4 d-flex justify-content-center align-items-center">
-                    <Card style={{ width: "100%  " }} className='p-2 shadow'>
-
-                        <Card.Body>
-                            <Card.Title>Card Title</Card.Title>
-                            <Card.Text className='mt-3'>
-                                Some quick example text to build on the card title and make up the
-                                bulk of the card's content.
-                            </Card.Text>
-                            <Button variant="danger" className='mt-2'>View More</Button>
-                        </Card.Body>
-                    </Card>
-                </div>
-
-
-
-            </div>
-
+            <NewsCards />
 
 
         </>
--- src/components/NewsCards.jsx
+++ src/components/NewsCards.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 function NewsCards() {
   return (
-    <div>NewsCards</div>
+    <>
+      <div className="row w-100 mt-3 mb-2 d-flex justify-content-center align-items-center p-4 ">
+
+        <div className="col-md-4 p-4 d-flex justify-content-center align-items-center">
+          <Card style={{ width: "100% " }} className='p-2 shadow'>
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text className='mt-3'>
+                Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+              </Card.Text>
+              <Button variant="danger" className='mt-2'>View More</Button>
+            </Card.Body>
+          </Card>
+          </div>
+      </div>
+
+    </>
   )
 }
 
