fb97361e47d5831fd77db3c9570d568ac081837b
Silpa Tom <silpatom@gmail.com>
2024-07-20 21:01:32 +0530

    ''

--- src/components/CategorizedView.jsx
+++ src/components/CategorizedView.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
+
 function CategorizedView() {
 
     return (
@@ -20,6 +21,17 @@ function CategorizedView() {
 
 
             <div className="row w-100 mt-3 mb-2 d-flex justify-content-center align-items-center p-4 ">
+
+            <Card style={{ width: "100% " }} className='p-2'>
+                        <Card.Body>
+                            <Card.Title>Card Title</Card.Title>
+                            <Card.Text className='mt-3'>
+                                Some quick example text to build on the card title and make up the
+                                bulk of the card's content.
+                            </Card.Text>
+                            <Button variant="danger" className='mt-2'>View More</Button>
+                        </Card.Body>
+                    </Card>
 
 
 
