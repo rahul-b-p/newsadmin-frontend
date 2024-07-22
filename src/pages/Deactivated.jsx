9afc0d8f643e55300a30b15b8d0fd8cbc303c139
Rahul-BP <rahulzantoz10@lbscek.ac.in>
2024-07-22 21:50:41 +0530

    errors cleared

--- src/pages/Deactivated.jsx
+++ src/pages/Deactivated.jsx
@@ -7,7 +7,7 @@ function Deactivated() {
   <h1 className='mt-5' style={{ marginLeft: "60px" }}>Deactive <span className='text-danger fw-bolder'>News</span>   </h1>
 
   <div className='w-25 border p-2 mb-5'>
-    <NewsCards/>
+    {/* <NewsCards/> */}
 
     <div className='d-flex justify-content-between p-2'>
       <button className='btn btn-success' >Activate</button>
