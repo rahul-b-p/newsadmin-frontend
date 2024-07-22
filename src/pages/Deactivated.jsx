3e9ab565eece819e42ad29ca38b77332603ff445
AshaMathew777 <pintubabu1999@gmail.com>
2024-07-22 11:57:17 +0530

    header

--- src/pages/Deactivated.jsx
+++ src/pages/Deactivated.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import NewsCards from '../components/NewsCards'
 
 function Deactivated() {
   return (
-    <div>Deactivated</div>
+  <>
+  <h1 className='mt-5' style={{ marginLeft: "60px" }}>Deactive <span className='text-danger fw-bolder'>News</span>   </h1>
+
+  <div className='w-25 border p-2 mb-5'>
+    <NewsCards/>
+
+    <div className='d-flex justify-content-between p-2'>
+      <button className='btn btn-success' >Activate</button>
+      <button className='btn btn-success'>Delete</button>
+    </div>
+  </div>
+  
+  </>
   )
 }
 
